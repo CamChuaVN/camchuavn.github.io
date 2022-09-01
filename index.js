6c9fe5b8973a42f7bc0f310ec644df8c461fe128
CamChua_VN <32415041+CamChuaVN@users.noreply.github.com>
2022-09-01 08:40:46 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ window.customElements.define("x-include", class Include extends HTMLElement {
     console.log(this.innerHTML = await fetchInfo.text());
   }
 });
+
+setTimeout(() => {
+  let id = null;
+  const elem = document.getElementById("loader");
+  const load1 = document.getElementById("loader_elem")
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 0.5);
+  function frame() {
+    if (pos === 800) {
+      clearInterval(id);
+      elem.parentNode.removeChild(elem)
+    } else {
+      pos += 8;
+      elem.style.top = pos + "px";
+    }
+  }
+}, 1200);
